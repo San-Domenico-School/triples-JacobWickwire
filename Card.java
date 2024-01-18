9a151b359f7d2e7f4d5ad20367b9e07655c547ed
JacobWickwire <144722936+JacobWickwire@users.noreply.github.com>
2024-01-18 10:22:57 -0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -18,6 +18,15 @@ public class Card extends Actor
    GreenfootImage selectedCardImage; 
    
    
+   Card(Shape shape, Color color, int numberOfShapes, int shading, GreenfootImage cardImage, GreenfootImage selectedCardImage)
+   {
+       super();
+   }
+   
+   public static void main(String[] args)
+   {
+       Card card = new Card(Card.Shape, Card.Color, int numberOfShapes, int shading, greenfoot.GreenfootImage, greenfoot.GreenfootImage);
+   }
    
     public enum Shape
    {
