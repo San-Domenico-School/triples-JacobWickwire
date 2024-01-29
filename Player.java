70a15b71c37525b0a85b1131e9ef9babe77075ed
JacobWickwire <144722936+JacobWickwire@users.noreply.github.com>
2024-01-29 11:05:56 -0800

    qqwertyuiop

---
+++ Player.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList; 
+
+/**
+ * a player is YOU! :) 
+ * 
+ * @author (Jacob Wickwire) 
+ * @version (1.0)
+ */
+public class Player extends Actor
+{
+    private Dealer dealer; 
+    private Card cardsSelected; 
+    private ArrayList<Card> cardsOnBoard; 
+    private ArrayList<Integer> selectedCardsIndex; 
+    
+}
